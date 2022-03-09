0b803a8a1f9730f3f287eefcb7a7f24302e7c66e
An Nguyen <haanngn501@gmail.com>
2022-03-09 08:23:30 +0100

    drew black rect, custom view class created

---
+++ MineSweeper/app/src/main/java/hu/ait/minesweeper/MainActivity.kt
@@ -0,0 +1,39 @@
+package hu.ait.minesweeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import hu.ait.minesweeper.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var flagOn = false
+
+        binding.toggleFlag.setOnClickListener {
+            if (flagOn == false) {
+                flagOn = true
+            } else {
+                flagOn = false
+            }
+            println(flagOn)
+        }
+
+    }
+
+//    public fun showText(message: String) {
+//        //Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+//        binding.tvMessage.text = message
+//    }
+//
+    public fun isFlagModeOn() : Boolean {
+        return binding.toggleFlag.isChecked
+    }
+
+}

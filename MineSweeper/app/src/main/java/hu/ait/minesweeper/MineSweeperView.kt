8fb81c36d482a76b83b39cfb3898b282637604ef
An Nguyen <haanngn501@gmail.com>
2022-03-09 09:00:28 +0100

    drawn 5x5 grids

--- MineSweeper/app/src/main/java/hu/ait/minesweeper/MineSweeperView.kt
+++ MineSweeper/app/src/main/java/hu/ait/minesweeper/MineSweeperView.kt
@@ -10,11 +10,17 @@ import android.view.View
 class MineSweeperView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private lateinit var paintBackgorund: Paint
+    private lateinit var paintLine: Paint
 
     init {
         paintBackgorund = Paint()
         paintBackgorund.color = Color.BLACK
         paintBackgorund.style = Paint.Style.FILL
+
+        paintLine = Paint()
+        paintLine.color = Color.WHITE
+        paintLine.style = Paint.Style.STROKE
+        paintLine.strokeWidth = 5f
     }
 
 
@@ -23,7 +29,32 @@ class MineSweeperView(context: Context?, attrs: AttributeSet?) : View(context, a
 
         canvas?.drawRect(
             0f, 0f, width.toFloat(),
-            height.toFloat(), paintBackgorund)
+            height.toFloat(), paintBackgorund
+        )
+
+        drawGameArea(canvas!!)
     }
+
+    private fun drawGameArea(canvas: Canvas) {
+        // border
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
+        // four horizontal lines
+        for (i in 1..4) {
+            canvas.drawLine(
+                0f, (i * height / 5).toFloat(), width.toFloat(), (i* height / 5).toFloat(),
+                paintLine
+            )
+        }
+
+        // four vertical lines
+        for (i in 1..4) {
+            canvas.drawLine(
+                (i * width / 5).toFloat(), 0f, (i * width / 5).toFloat(), height.toFloat(),
+                paintLine
+            )
+        }
+
+    }
+
 
 }

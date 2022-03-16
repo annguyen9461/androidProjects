d64cbf3abc17a31a5df5b1ec85d19a446587861b
An Nguyen <haanngn501@gmail.com>
2022-03-16 01:33:10 +0100

    losing mechanism only works when clicking twice on mine cell. snackbar working except for the lil message bar not showing

--- MineSweeper/app/src/main/java/hu/ait/minesweeper/MainActivity.kt
+++ MineSweeper/app/src/main/java/hu/ait/minesweeper/MainActivity.kt
@@ -35,32 +35,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-//    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-//    fun revealCard() {
-//        val x = binding.cardView.getRight()
-//        val y = binding.cardView.getBottom()
-//
-//        val startRadius = 0
-//        val endRadius = Math.hypot(binding.cardView.getWidth().toDouble(),
-//            binding.cardView.getHeight().toDouble()).toInt()
-//
-//        val anim = ViewAnimationUtils.createCircularReveal(
-//            binding.cardView,
-//            x,
-//            y,
-//            startRadius.toFloat(),
-//            endRadius.toFloat()
-//        )
-//
-//        binding.cardView.setVisibility(View.VISIBLE)
-//        anim.start()
-//    }
-
-//    public fun showText(message: String) {
-//        //Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-//        binding.tvMessage.text = message
-//    }
-
     public fun isFlagModeOn(): Boolean {
         print(binding.toggleFlag.isChecked)
         return binding.toggleFlag.isChecked

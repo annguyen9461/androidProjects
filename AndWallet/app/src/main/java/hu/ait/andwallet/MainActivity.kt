ce77592f1271dc6ec9d386c09edb814951b0b6b8
An Nguyen <haanngn501@gmail.com>
2022-03-24 23:40:18 +0100

    layout inflater initial

--- AndWallet/app/src/main/java/hu/ait/andwallet/MainActivity.kt
+++ AndWallet/app/src/main/java/hu/ait/andwallet/MainActivity.kt
@@ -3,6 +3,7 @@ package hu.ait.andwallet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hu.ait.andwallet.databinding.ActivityMainBinding
+import hu.ait.andwallet.databinding.MoneyRowBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -11,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener() {
+            addNewMoneyItem()
+        }
+    }
+
+    private fun addNewMoneyItem() {
+        val moneyRow = MoneyRowBinding.inflate(layoutInflater)
+        binding.root.addView(moneyRow.root)
     }
 }

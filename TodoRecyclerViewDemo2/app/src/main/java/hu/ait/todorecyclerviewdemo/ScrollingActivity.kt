b8ff5f7c56985248196bb0b87a03913e679bbfbd
An Nguyen <haanngn501@gmail.com>
2022-04-10 13:04:30 +0200

    swipe touch enabled

--- TodoRecyclerViewDemo2/app/src/main/java/hu/ait/todorecyclerviewdemo/ScrollingActivity.kt
+++ TodoRecyclerViewDemo2/app/src/main/java/hu/ait/todorecyclerviewdemo/ScrollingActivity.kt
@@ -9,10 +9,12 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import hu.ait.todorecyclerviewdemo.adapter.TodoAdapter
 import hu.ait.todorecyclerviewdemo.data.Todo
 import hu.ait.todorecyclerviewdemo.databinding.ActivityScrollingBinding
 import hu.ait.todorecyclerviewdemo.dialog.TodoDialog
+import hu.ait.todorecyclerviewdemo.touch.TodoReyclerTouchCallback
 
 class ScrollingActivity : AppCompatActivity(), TodoDialog.TodoHandler {
 
@@ -36,11 +38,15 @@ class ScrollingActivity : AppCompatActivity(), TodoDialog.TodoHandler {
         adapter = TodoAdapter(this)
         binding.recyclerTodo.adapter = adapter
 
-        val decorator =  DividerItemDecoration(this,
-            DividerItemDecoration.VERTICAL)
-        binding.recyclerTodo.addItemDecoration(decorator)
+//        val decorator =  DividerItemDecoration(this,
+//            DividerItemDecoration.VERTICAL)
+//        binding.recyclerTodo.addItemDecoration(decorator)
 
-        binding.recyclerTodo.layoutManager = GridLayoutManager(this, 2)
+//        binding.recyclerTodo.layoutManager = GridLayoutManager(this, 2)
+
+        val touchCallbakList = TodoReyclerTouchCallback(adapter)
+        val itemTouchHelper = ItemTouchHelper(touchCallbakList)
+        itemTouchHelper.attachToRecyclerView(binding.recyclerTodo)
     }
 
     override fun todoCreated(todo: Todo) {

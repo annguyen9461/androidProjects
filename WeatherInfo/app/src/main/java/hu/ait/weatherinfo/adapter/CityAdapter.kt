5bac4e67c27195be1e4a3b14065e1e5b1c4ecd6c
An Nguyen <haanngn501@gmail.com>
2022-05-03 17:29:46 +0200

    used view binding for adapter class for shorter code

--- WeatherInfo/app/src/main/java/hu/ait/weatherinfo/adapter/CityAdapter.kt
+++ WeatherInfo/app/src/main/java/hu/ait/weatherinfo/adapter/CityAdapter.kt
@@ -8,36 +8,38 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hu.ait.weatherinfo.R
 import hu.ait.weatherinfo.data.City
+import hu.ait.weatherinfo.databinding.CityRowBinding
 
 class CityAdapter : RecyclerView.Adapter<CityAdapter.ViewHolder> {
-
     var cityItems = mutableListOf<City>(
         City("New York"),
         City("Budapest")
     )
-
-    override fun getItemCount(): Int {
-        return cityItems.size
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(
-            R.layout.city_row, parent, false
-        )
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val city = cityItems[position]
-        holder.tvCityName.text = city.cityName
-    }
 
     val context : Context
     constructor(context: Context) : super() {
         this.context = context
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvCityName: TextView = itemView.findViewById(R.id.tvCityName)
+    override fun getItemCount(): Int {
+        return cityItems.size
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cityBinding = CityRowBinding.inflate(LayoutInflater.from(context),
+            parent, false)
+        return ViewHolder(cityBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val city = cityItems[position]
+        holder.bind(city)
+    }
+
+    inner class ViewHolder(var binding: CityRowBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(city: City) {
+            binding.tvCityName.text = city.cityName
+        }
+    }
+
 }

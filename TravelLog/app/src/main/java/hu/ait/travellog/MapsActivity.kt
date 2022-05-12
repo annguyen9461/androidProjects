3a231ad9cb92c4ae204030b4fd71a972522e13fd
An Nguyen <haanngn501@gmail.com>
2022-05-12 15:54:58 +0200

    location permission feature working

--- TravelLog/app/src/main/java/hu/ait/travellog/MapsActivity.kt
+++ TravelLog/app/src/main/java/hu/ait/travellog/MapsActivity.kt
@@ -1,7 +1,15 @@
 package hu.ait.travellog
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,12 +20,17 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import hu.ait.travellog.databinding.ActivityMapsBinding
+import java.util.*
+import kotlin.concurrent.thread
 import kotlin.random.Random
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+    MyLocationManager.OnNewLocationAvailable {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var myLocationManager: MyLocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +38,62 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        myLocationManager = MyLocationManager(this, this)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        requestNeededPermission()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    fun requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                101
+            )
+        } else {
+            // we have the permission
+            myLocationManager.startLocationMonitoring()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            101 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "ACCESS_FINE_LOCATION perm granted", Toast.LENGTH_SHORT)
+                        .show()
+
+                    myLocationManager.startLocationMonitoring()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "ACCESS_FINE_LOCATION perm NOT granted", Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        myLocationManager.stopLocationMonitoring()
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -62,15 +116,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val marker = mMap.addMarker(
                 MarkerOptions()
                     .position(it)
-                    .title("Demo")
-                    .snippet("Demo text")
             )
             marker!!.isDraggable = true
         }
+
 
         // Add a marker in Sydney and move the camera
 //        val sydney = LatLng(-34.0, 151.0)
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onNewLocation(location: Location) {
+        thread {
+            try {
+                val gc = Geocoder(this, Locale.getDefault())
+                var addrs: List<Address> =
+                    gc.getFromLocation(location.latitude, location.longitude, 3)
+                val addr =
+                    "${addrs[0].getAddressLine(0)}, ${addrs[0].getAddressLine(1)}"
+
+                runOnUiThread {
+                    mMap.addMarker(
+                        MarkerOptions()
+                            .position(LatLng(location.latitude, location.longitude))
+                            .title(addr)
+                            .snippet(addr)
+                    )
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this@MapsActivity, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ TravelLog/app/src/main/java/hu/ait/travellog/MyLocationManager.kt
@@ -0,0 +1,52 @@
+package hu.ait.travellog
+
+import android.content.Context
+import android.location.Location
+import android.os.Looper
+import com.google.android.gms.location.*
+import kotlin.jvm.Throws
+
+class MyLocationManager(context: Context,
+                        private val onNewLocationHandler: OnNewLocationAvailable) {
+
+    interface OnNewLocationAvailable {
+        fun onNewLocation(location: Location)
+    }
+
+    private val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
+
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            onNewLocationHandler.onNewLocation(locationResult.lastLocation)
+        }
+    }
+
+    @Throws(SecurityException::class)
+    fun getLastLocation(callback: (Location) -> Unit)  {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                if (location != null) {
+                    callback(location)
+                }
+            }
+    }
+
+    @Throws(SecurityException::class)
+    fun startLocationMonitoring() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 10000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCallback, Looper.myLooper()!!)
+
+    }
+
+    @Throws(SecurityException::class)
+    fun stopLocationMonitoring() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+}

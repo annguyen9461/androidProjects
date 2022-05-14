399374631ebbcb47aa53a51f33d487da42ee0679
An Nguyen <haanngn501@gmail.com>
2022-05-15 00:47:33 +0200

    posts are displayed

--- TravelLog/app/src/main/java/hu/ait/travellog/MarkerDetails.kt
+++ TravelLog/app/src/main/java/hu/ait/travellog/MarkerDetails.kt
@@ -2,19 +2,22 @@ package hu.ait.travellog
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
 import hu.ait.travellog.adapter.PostsAdapter
+import hu.ait.travellog.data.Post
 import hu.ait.travellog.databinding.ActivityMarkerDetailsBinding
 
 class MarkerDetails : AppCompatActivity() {
 
     private lateinit var binding: ActivityMarkerDetailsBinding
-
     private lateinit var postsAdapter: PostsAdapter
+    private var listenerReg: ListenerRegistration? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +34,45 @@ class MarkerDetails : AppCompatActivity() {
         postsAdapter = PostsAdapter(this,
             FirebaseAuth.getInstance().currentUser!!.uid)
         binding.recyclerPosts.adapter = postsAdapter
+
+        initFirebaseQuery()
     }
+
+    private fun initFirebaseQuery() {
+        val queryRef =
+            FirebaseFirestore.getInstance().collection(
+                CreatePostActivity.POSTS_COLLECTION)
+
+        val eventListener = object : EventListener<QuerySnapshot> {
+            override fun onEvent(querySnapshot: QuerySnapshot?, e: FirebaseFirestoreException?) {
+                if (e != null) {
+                    Toast.makeText(
+                        this@MarkerDetails, "Error: ${e.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return
+                }
+
+                for (docChange in querySnapshot?.getDocumentChanges()!!) {
+                    if (docChange.type == DocumentChange.Type.ADDED) {
+                        val post = docChange.document.toObject(Post::class.java)
+                        postsAdapter.addPost(post, docChange.document.id)
+                    } else if (docChange.type == DocumentChange.Type.REMOVED) {
+                        postsAdapter.removePostByKey(docChange.document.id)
+                    } else if (docChange.type == DocumentChange.Type.MODIFIED) {
+
+                    }
+                }
+
+            }
+        }
+
+        listenerReg = queryRef.addSnapshotListener(eventListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        listenerReg?.remove()
+    }
+
 }
